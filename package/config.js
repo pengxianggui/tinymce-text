138541272bd83c845fda6c352a1eef53e238a2d0
pengxg <pengxianggui@outlook.com>
2022-05-17 16:50:55 +0800

    update: 更换tinymceCDN地址;顺道升级到6.x

---
+++ package/config.js
@@ -0,0 +1,10 @@
+
+export const plugins = ['advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+    'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
+    'media', 'table', 'emoticons', 'template', 'help'];
+
+export const toolbar = ['undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | ' +
+'bullist numlist outdent indent blockquote | link image | print preview media fullscreen | ' +
+'forecolor backcolor emoticons | help'];
+
+export const menubar = "file edit insert view format table"
